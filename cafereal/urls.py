b2cc39e5217a2ae5a9ef641ac6165dc4a90d6846
rbloor97 <rbloor97@hotmail.com>
2019-08-05 15:28:02 -0500

    apirest prueba

--- cafereal/urls.py
+++ cafereal/urls.py
@@ -1,7 +1,21 @@
-from django.contrib import admin
-from django.urls import include,path
+from django.urls import include, path
+from rest_framework import routers
+from cafereal import views
+
+router = routers.DefaultRouter()
+router.register(r'Contacto', views.ContactoViewSet)
+router.register(r'Persona', views.PersonaViewSet)
+router.register(r'Cliente', views.ClienteViewSet)
+router.register(r'Consumidor', views.ConsumidorViewSet)
+router.register(r'Orden_Menu', views.Orden_MenuViewSet)
+router.register(r'Categoria', views.CategoriaViewSet)
+router.register(r'Producto', views.ProductoViewSet)
+router.register(r'Detalle_orden_menu', views.Detalle_orden_menuViewSet)
+router.register(r'Combo', views.ComboViewSet)
+router.register(r'ComboProducto', views.ComboProductoViewSet)
 
 urlpatterns = [
     path('myapp/', include('myapp.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,81 @@
 from django.http import HttpResponse
+from django.contrib.auth.models import *
+from rest_framework import viewsets
+from proyecto2p.myapp.serializers import *
+
 def index(request):
     return HttpResponse("Hello, estamos empezando Café Real")
 # Create your views here.
+
+class PersonaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Persona.objects.all()
+    serializer_class = PersonaSerializer
+
+
+class ContactoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Contacto.objects.all()
+    serializer_class = ContactoSerializer
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+class ConsumidorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Consumidor.objects.all()
+    serializer_class = ConsumidorSerializer
+
+class Orden_MenuViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Orden_Menu.objects.all()
+    serializer_class = Orden_MenuSerializer
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class ProductoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+
+class Detalle_orden_menuViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Detalle_orden_menu.objects.all()
+    serializer_class = Detalle_orden_menuSerializer
+
+
+class ComboViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Combo.objects.all()
+    serializer_class = ComboSerializer
+
+class ComboProductoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = ComboProducto.objects.all()
+    serializer_class = ComboProductoSerializer
+
